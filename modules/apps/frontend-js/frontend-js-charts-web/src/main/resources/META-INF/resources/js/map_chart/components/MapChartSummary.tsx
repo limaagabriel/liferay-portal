0db7c4a750f55146e16e7647115d67d585734a99
Gabriel Lima <gabriel.lima@liferay.com>
2026-07-06 21:02:29 -0300

    LPD-96000 Add MapChart root, markers shell, and a11y scaffold

---
+++ modules/apps/frontend-js/frontend-js-charts-web/src/main/resources/META-INF/resources/js/map_chart/components/MapChartSummary.tsx
@@ -0,0 +1,35 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import React from 'react';
+
+import {MapDatum} from '../types/MapDatum';
+import {toPercent} from '../utils/percent';
+
+interface MapChartSummaryProps {
+	data: MapDatum[];
+	id: string;
+	total: number;
+}
+
+export default function MapChartSummary({
+	data,
+	id,
+	total,
+}: MapChartSummaryProps) {
+	return (
+		<p className="chart-map-summary sr-only" id={id}>
+			{data
+				.map(
+					(datum) =>
+						`${datum.label}: ${datum.value} (${toPercent(
+							datum.value,
+							total
+						)}%). `
+				)
+				.join('')}
+		</p>
+	);
+}
