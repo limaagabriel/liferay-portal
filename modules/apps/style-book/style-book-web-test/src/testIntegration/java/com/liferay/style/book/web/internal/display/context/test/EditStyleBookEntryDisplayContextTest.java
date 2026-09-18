aa8269d9484b95face7884555acae95e7f709a61
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2026-09-18 11:50:05 +0200

    LPD-105532 Test that bare-name token values are normalized to namespaced on load

--- modules/apps/style-book/style-book-web-test/src/testIntegration/java/com/liferay/style/book/web/internal/display/context/test/EditStyleBookEntryDisplayContextTest.java
+++ modules/apps/style-book/style-book-web-test/src/testIntegration/java/com/liferay/style/book/web/internal/display/context/test/EditStyleBookEntryDisplayContextTest.java
@@ -7,12 +7,14 @@ package com.liferay.style.book.web.internal.display.context.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.frontend.token.definition.util.FrontendTokenDefinitionUtil;
+import com.liferay.layout.test.util.LayoutTestUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalService;
@@ -24,6 +26,7 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.LocaleUtil;
@@ -49,6 +52,7 @@ import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
  * @author Gabriel Lima
+ * @author Ambrín Chaudhary
  */
 @RunWith(Arquillian.class)
 public class EditStyleBookEntryDisplayContextTest {
@@ -63,6 +67,21 @@ public class EditStyleBookEntryDisplayContextTest {
 	@Before
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
+
+		_layout = LayoutTestUtil.addTypePortletLayout(_group.getGroupId());
+
+		_themeDisplay = new ThemeDisplay();
+
+		_themeDisplay.setCompany(
+			_companyLocalService.getCompany(TestPropsValues.getCompanyId()));
+		_themeDisplay.setLayout(_layout);
+		_themeDisplay.setLocale(LocaleUtil.getDefault());
+		_themeDisplay.setPermissionChecker(
+			PermissionThreadLocal.getPermissionChecker());
+		_themeDisplay.setRealUser(TestPropsValues.getUser());
+		_themeDisplay.setScopeGroupId(_group.getGroupId());
+		_themeDisplay.setSiteGroupId(_group.getGroupId());
+		_themeDisplay.setUser(TestPropsValues.getUser());
 	}
 
 	@Test
@@ -135,6 +154,143 @@ public class EditStyleBookEntryDisplayContextTest {
 		Assert.assertTrue(frontendTokenNames.contains(frontendTokenName));
 	}
 
+	@Test
+	public void testGetStyleBookEditorData() throws Exception {
+		_testGetStyleBookEditorDataWithBareNameTokenKey();
+		_testGetStyleBookEditorDataWithBothBareAndNamespacedTokenKeys();
+		_testGetStyleBookEditorDataWithNamespacedTokenKey();
+	}
+
+	private StyleBookEntry _addStyleBookEntry(
+			String frontendTokensValues, String themeId)
+		throws Exception {
+
+		return _styleBookEntryLocalService.addStyleBookEntry(
+			null, TestPropsValues.getUserId(), _group.getGroupId(), false,
+			StringPool.BLANK, frontendTokensValues,
+			RandomTestUtil.randomString(), StringPool.BLANK, themeId,
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+	}
+
+	private JSONObject _getFrontendTokensValuesJSONObject(
+			StyleBookEntry styleBookEntry)
+		throws Exception {
+
+		MockLiferayPortletRenderRequest mockLiferayPortletRenderRequest =
+			new MockLiferayPortletRenderRequest();
+
+		mockLiferayPortletRenderRequest.addParameter(
+			"styleBookEntryId",
+			String.valueOf(styleBookEntry.getStyleBookEntryId()));
+		mockLiferayPortletRenderRequest.addParameter("redirect", "/");
+		mockLiferayPortletRenderRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, _themeDisplay);
+
+		_mvcRenderCommand.render(
+			mockLiferayPortletRenderRequest,
+			new MockLiferayPortletRenderResponse());
+
+		Map<String, Object> editorData = ReflectionTestUtil.invoke(
+			mockLiferayPortletRenderRequest.getAttribute(
+				"com.liferay.style.book.web.internal.display.context." +
+					"EditStyleBookEntryDisplayContext"),
+			"getStyleBookEditorData", new Class<?>[0]);
+
+		return (JSONObject)editorData.get("frontendTokensValues");
+	}
+
+	private void _testGetStyleBookEditorDataWithBareNameTokenKey()
+		throws Exception {
+
+		String themeId = RandomTestUtil.randomString();
+		String tokenName = RandomTestUtil.randomString();
+		String tokenValue = RandomTestUtil.randomString();
+
+		StyleBookEntry styleBookEntry = _addStyleBookEntry(
+			JSONUtil.put(
+				tokenName, JSONUtil.put("value", tokenValue)
+			).toString(),
+			themeId);
+
+		JSONObject frontendTokensValuesJSONObject =
+			_getFrontendTokensValuesJSONObject(styleBookEntry);
+
+		Assert.assertFalse(frontendTokensValuesJSONObject.has(tokenName));
+		Assert.assertTrue(
+			frontendTokensValuesJSONObject.has(
+				themeId + StringPool.COLON + tokenName));
+		Assert.assertEquals(
+			tokenValue,
+			frontendTokensValuesJSONObject.getJSONObject(
+				themeId + StringPool.COLON + tokenName
+			).getString(
+				"value"
+			));
+	}
+
+	private void _testGetStyleBookEditorDataWithBothBareAndNamespacedTokenKeys()
+		throws Exception {
+
+		String themeId = RandomTestUtil.randomString();
+		String tokenName = RandomTestUtil.randomString();
+
+		String namespacedKey = themeId + StringPool.COLON + tokenName;
+
+		String bareValue = RandomTestUtil.randomString();
+		String namespacedValue = RandomTestUtil.randomString();
+
+		StyleBookEntry styleBookEntry = _addStyleBookEntry(
+			JSONUtil.put(
+				namespacedKey, JSONUtil.put("value", namespacedValue)
+			).put(
+				tokenName, JSONUtil.put("value", bareValue)
+			).toString(),
+			themeId);
+
+		JSONObject frontendTokensValuesJSONObject =
+			_getFrontendTokensValuesJSONObject(styleBookEntry);
+
+		Assert.assertFalse(frontendTokensValuesJSONObject.has(tokenName));
+		Assert.assertTrue(frontendTokensValuesJSONObject.has(namespacedKey));
+		Assert.assertEquals(
+			namespacedValue,
+			frontendTokensValuesJSONObject.getJSONObject(
+				namespacedKey
+			).getString(
+				"value"
+			));
+	}
+
+	private void _testGetStyleBookEditorDataWithNamespacedTokenKey()
+		throws Exception {
+
+		String themeId = RandomTestUtil.randomString();
+		String tokenName = RandomTestUtil.randomString();
+
+		String namespacedKey = themeId + StringPool.COLON + tokenName;
+
+		String tokenValue = RandomTestUtil.randomString();
+
+		StyleBookEntry styleBookEntry = _addStyleBookEntry(
+			JSONUtil.put(
+				namespacedKey, JSONUtil.put("value", tokenValue)
+			).toString(),
+			themeId);
+
+		JSONObject frontendTokensValuesJSONObject =
+			_getFrontendTokensValuesJSONObject(styleBookEntry);
+
+		Assert.assertFalse(frontendTokensValuesJSONObject.has(tokenName));
+		Assert.assertTrue(frontendTokensValuesJSONObject.has(namespacedKey));
+		Assert.assertEquals(
+			tokenValue,
+			frontendTokensValuesJSONObject.getJSONObject(
+				namespacedKey
+			).getString(
+				"value"
+			));
+	}
+
 	private static final String _THEME_ID_CLASSIC = "classic_WAR_classictheme";
 
 	@Inject
@@ -143,6 +299,8 @@ public class EditStyleBookEntryDisplayContextTest {
 	@DeleteAfterTestRun
 	private Group _group;
 
+	private Layout _layout;
+
 	@Inject(
 		filter = "component.name=com.liferay.style.book.web.internal.portlet.action.EditStyleBookEntryMVCRenderCommand"
 	)
@@ -150,5 +308,7 @@ public class EditStyleBookEntryDisplayContextTest {
 
 	@Inject
 	private StyleBookEntryLocalService _styleBookEntryLocalService;
+
+	private ThemeDisplay _themeDisplay;
 
 }
